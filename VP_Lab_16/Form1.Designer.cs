6e6d7338d9ca91a772c15ced8acee92a0af09a50
musamagujjar <48413965+musamagujjar@users.noreply.github.com>
2019-10-16 15:52:11 +0500

    Add project files.

---
+++ VP_Lab_16/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace VP_Lab_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(363, 21);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 201);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(722, 208);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ VP_Lab_16/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_Lab_16
+{
+    public partial class Form1 : Form
+    {
+        DataTable dtname = new DataTable();
+        DataTable dtcategory = new DataTable();
+
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dtname.Columns.Add("Name");
+            dtname.Columns.Add("Roll");
+            dtname.Columns.Add("Department");
+            dtname.Columns.Add("Semester");
+            dtname.Columns.Add("Category");
+
+
+            dtname.Rows.Add("Usama", "11421", "Engg" ,"6th" ,"A");
+            dtname.Rows.Add("Ali", "11420", "Engg", "6th", "B");
+            dtname.Rows.Add("Haider", "11422", "Engg", "6th", "C");
+            dtname.Rows.Add("Shahnawaz", "11426", "Engg", "6th", "D");
+            dtname.Rows.Add("Kabeer", "11424", "Engg", "6th", "E");
+            dtname.Rows.Add("Awais", "11425", "Engg", "6th", "F");
+
+
+
+            
+         
+            dataGridView1.DataSource = dtname;
+        }
+
+        
+
+       /* private void button1_Click(object sender, EventArgs e)
+        {
+                   //dtname.DefaultView.RowFilter = "Name like '" + textBox1.Text + "%' and Category ='" + comboBox2 + "'";
+        }*/
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        
+            dtname.DefaultView.RowFilter = "Name like '" + textBox1.Text + "%' and Category ='" + comboBox2.SelectedItem + "'";
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dtname.DefaultView.RowFilter = "Name like '" + textBox1.Text + "%'";
+        }
+    }
+}
